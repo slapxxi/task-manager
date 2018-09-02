b53c738b694be408086d5e06a0157fe8f5bd4e92
slavapavlutin <sl.pavlutin@gmail.com>
2018-09-02 10:18:57 +0300

    Add an additional test case for `themeToCSSVariables` tests

--- src/lib/themeToCSSVariables.spec.ts
+++ src/lib/themeToCSSVariables.spec.ts
@@ -1,5 +1,10 @@
 import themeToCSSVariables from './themeToCSSVariables';
 
+it('does nothing with empty theme', () => {
+  const result = themeToCSSVariables({});
+  expect(result).toEqual('');
+});
+
 it('converts theme to CSS variables', () => {
   const result = themeToCSSVariables({
     primaryColor: 'red',
